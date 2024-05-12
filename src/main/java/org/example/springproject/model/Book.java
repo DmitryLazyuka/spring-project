3ae02ddaf75a2476bbe5728b00876ff0b1b39281
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-05-12 16:04:13 +0300

    Removed @NoArgsConstructor, fixed ddl-auto parameter

--- src/main/java/org/example/springproject/model/Book.java
+++ src/main/java/org/example/springproject/model/Book.java
@@ -8,12 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "books")
-@NoArgsConstructor
 @Getter
 @Setter
 public class Book {
