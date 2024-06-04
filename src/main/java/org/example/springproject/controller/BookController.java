6a0245667ce98e82e2ae6e007affba44a49a4ee0
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-06-04 21:04:25 +0300

    Global Exception Handler Task

--- src/main/java/org/example/springproject/controller/BookController.java
+++ src/main/java/org/example/springproject/controller/BookController.java
@@ -1,5 +1,6 @@
 package org.example.springproject.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.springproject.dto.BookDto;
@@ -33,12 +34,13 @@ public class BookController {
     }
 
     @PostMapping()
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @PutMapping("/{id}")
-    public BookDto updateBook(@PathVariable Long id, @RequestBody CreateBookRequestDto bookDto) {
+    public BookDto updateBook(@PathVariable Long id,
+                              @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.update(id, bookDto);
     }
 
