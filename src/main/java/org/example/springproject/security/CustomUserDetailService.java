5633c6a351e3f21a4c7999cc27ac45a418441d21
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-07-17 10:19:56 +0300

    Replaced Exception in CustomUserDetailService, removed unused UserService in Auth Controller

--- src/main/java/org/example/springproject/security/CustomUserDetailService.java
+++ src/main/java/org/example/springproject/security/CustomUserDetailService.java
@@ -1,10 +1,10 @@
 package org.example.springproject.security;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springproject.exception.EntityNotFoundException;
 import org.example.springproject.repository.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +13,8 @@ public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
+    public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("Can't find user by email " + email));
+                () -> new EntityNotFoundException("Can't find user by email " + email));
     }
 }
